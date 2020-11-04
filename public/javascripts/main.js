4ed8d1c6b73cfcda1c9eb227de8deded36846666
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-04 14:19:54 +0100

    showing elements function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,4 @@
-console.log("Hej");
+console.log("Hej ( ͡° ͜ʖ ͡°)");
 closeForm();
 
 var JSONString = {
@@ -13,7 +13,23 @@ function openForm() {
     document.getElementById("addForm").style.display = "none";
   }
   
-  function closeForm() {
+function closeForm() {
     document.getElementById("newNote").style.display = "none";
     document.getElementById("addForm").style.display = "block";
-  }
+}
+
+function loadNotes(){
+    var div = document.createElement("div");
+    var header = document.createElement("h4");
+    var node = document.createTextNode("this is a test");
+    header.appendChild(node);
+    //p tag
+    var p = document.createElement("p");
+    var pNode = document.createTextNode("This is insider test");
+    p.appendChild(pNode);
+    //lägg in i diven
+    div.appendChild(header);
+    div.appendChild(p);
+    var element = document.getElementById("notes");
+    element.appendChild(div);
+}
