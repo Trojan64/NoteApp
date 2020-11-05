033c600961db83316b5dddaa4e25cd4c2facbec8
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-05 16:33:33 +0100

    added simple Exit button when reading Note

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,6 +17,10 @@ function closeForm() {
 }
 
 function loadNotes(){
+    //hide specifik note
+    document.getElementById("readNote").style.display = "none";
+    //show All Notes
+    document.getElementById("notes").style.display = "grid";
     document.getElementById("notes").innerHTML = "";
     //add new Note button in same div
     var div = document.createElement("div");
@@ -45,6 +49,9 @@ function loadNotes(){
 
 function loadSpecificNote(id) {
     console.log("Load note: " + id);
+    //show readNotes
+    document.getElementById("readNote").style.display = "block";
+    document.getElementById("readNote").innerHTML = "";
     //Title
     var Title = document.createElement("p");
     var TitleNode = document.createTextNode(JSONArray[id].name);
@@ -53,10 +60,16 @@ function loadSpecificNote(id) {
     var Note = document.createElement("p");
     var NoteNode = document.createTextNode(JSONArray[id].text);
     Note.appendChild(NoteNode);
+    //exit button
+    var Exit = document.createElement("div");
+    var ExitNode = document.createTextNode("X");
+    Exit.appendChild(ExitNode);
+    Exit.setAttribute("onclick", "loadNotes()");
     //add to readNote div
     var element = document.getElementById("readNote");
     element.appendChild(Title);
     element.appendChild(Note);
+    element.appendChild(Exit);
     //hide the other divs
     document.getElementById("notes").style.display = "none";
 }
