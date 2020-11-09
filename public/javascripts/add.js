68962763456d936a03c38df3e96637941e1d028d
viktorx64 <viktor.sundqvist2@gmail.com>
2020-11-09 17:10:11 +0100

    fixed empty title error

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -2,11 +2,20 @@ function addNote() {
     var Title = document.getElementById("title").value;
     var text = document.getElementById("text").value;
     var id = JSONArray.length;
-    var JSONString = {
-        "name": Title,
-        "text": text,
-        "id": id
-    };
+    if (Title != "") {
+        var JSONString = {
+            "name": Title,
+            "text": text,
+            "id": id
+        };
+    }
+    else {
+        var JSONString = {
+            "name": "[UNNAMNED]",
+            "text": text,
+            "id": id
+        };
+    }
     JSONArray.push(JSONString);
     closeForm();
     saveSNote();
