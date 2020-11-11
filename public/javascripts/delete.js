2302d4b9a385253656c15b2822e4f0ce58666c84
Max Runesson <Max@maxRunesson.se>
2020-11-11 15:31:52 +0100

    added confirm button on delete

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,8 +1,11 @@
 function deleteNote(id) {     //Delete Note
-    if(id!=0) {     //cant delete tutorial
-        JSONArray.splice(id, 1);
-        for (var i = 0; i < JSONArray.length; i++) {    //Fixar nya id
-            JSONArray[i].id = i;
+
+    if (confirm("Are you sure you want to delete this note?")) {
+        if(id!=0) {     //cant delete tutorial
+            JSONArray.splice(id, 1);
+            for (var i = 0; i < JSONArray.length; i++) {    //Fixar nya id
+                JSONArray[i].id = i;
+            }
         }
     }
     saveSNote();
